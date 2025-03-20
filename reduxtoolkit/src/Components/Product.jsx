30c841b1789e3838f1c40501209659ce092e593d
Prakashankith <sirankithbp1999@gmail.com>
2025-03-20 21:31:24 +0530

    initial commit

---
+++ reduxtoolkit/src/Components/Product.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+function Product() {
+  const [products, getProducts] = useState([]);
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((data) => data.json())
+      .then((result) => getProducts(result));
+  }, []);
+  return (
+    <>
+      <h1>Product dashboard</h1>;{JSON.stringify(products)}
+    </>
+  );
+}
+
+export default Product;
